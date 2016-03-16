cd3611646785c054f5a68bf4c836dab9b9708c52
juanshishido <juanshishido@cal.berkeley.edu>
2016-03-16 11:39:58 -0700

    refactor download script to use a loop

--- assignments/assignment03/data/get_data.sh
+++ assignments/assignment03/data/get_data.sh
@@ -1,13 +1,12 @@
 #!bin/bash
 
-if test -a 'logreg_coefficients.txt'; then
-    echo "log_regcoefficients.txt has already been downloaded"
-else
-    wget https://github.com/dbamman/dds/raw/master/homework/hw3/logreg_coefficients.txt
-fi
+FILES="logreg_coefficients.txt test_movies.txt"
 
-if test -a 'test_movies.txt'; then
-    echo "test_movies.txt has already been downloaded"
-else
-    wget https://github.com/dbamman/dds/raw/master/homework/hw3/test_movies.txt
-fi
+for f in $FILES
+do
+    if test -a $f; then
+        echo "$f has already been downloaded"
+    else
+        wget https://github.com/dbamman/dds/raw/master/homework/hw3/$f
+    fi
+done
